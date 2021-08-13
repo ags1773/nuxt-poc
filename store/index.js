0a119c9928365318662dd2774965f965d4baa78d
ags1773 <amogh@quintype.com>
2021-08-13 15:09:24 +0530

    slight inprovement in config fetching logic

--- store/index.js
+++ store/index.js
@@ -4,14 +4,16 @@ export const state = () => ({
 
 export const mutations = {
   setConfig(state, config) {
-    if (!state.config) state.config = config;
+    state.config = config;
   },
 };
 
 export const actions = {
-  async FETCH_CONFIG({ commit }) {
-    const config = await this.$http.$get("/api/v1/config");
-    commit("setConfig", config);
+  async FETCH_CONFIG({ commit, state }) {
+    if (!state.config) {
+      const config = await this.$http.$get("/api/v1/config");
+      commit("setConfig", config);
+    }
   },
   // async nuxtServerInit({ dispatch }, { req }) {
   //   console.log("**** req >> ", req);
